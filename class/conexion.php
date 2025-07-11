5224a7e103efd7417e392bad76a78753e66b5796
Rokbell/espinaza <roallamu@gmail.com>
2025-07-11 14:43:26 -0600

    agregando conexion

---
+++ class/conexion.php
@@ -0,0 +1,18 @@
+<?php
+    $servername = "localhost";
+    $username = "rok";
+    $password = "admin";
+    $dbname = "mi_base";
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
+    // Check connection
+    if (!$conn) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    echo "Connected successfully";
+
+  
+
+?>
